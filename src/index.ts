83bea75cd15cf2ee643c9482eda55dcf81cad659
Keito Yamakawa <keito.yamakawa@smail.astate.edu>
2023-02-01 16:12:41 -0600

    Stubbs out Enndpoints

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,20 @@
-import express, {Express} from 'express';
+import express, { Express } from 'express';
 import { notImplemented } from './controllers/NotImplementedController';
 
-const app: Express= express();
+const app: Express = express();
 const PORT = 8019;
 
 // function handlelistenevent(): void {
-//     console.log(`Server listening on https://localhost:${PORT}`);
+//     console.log(`Server listening on http://localhost:${PORT}`);
 // };
 
 // app.listen(PORT, handlelistenevent);
 
 app.get('/api/students', notImplemented);
 app.post('/api/students', notImplemented);
-
-// GET https://localhost:8019/api/customer/Keito //(WildCard)
+// GET http://localhost:8019/api/customer/Keito //(WildCard)
 app.get('/api/students/:studentName', notImplemented);
 
-
-
-app.listen(PORT, () =>{
-    console.log(`Server listening on https://localhost:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
 });
